17b8ab83e53eec562706c2cf505ca9b8727b1042
aetheryx <zain1@live.nl>
2017-11-29 22:34:32 +0100

    cleanup

--- reminder-manager/commandos/commandos.py
+++ reminder-manager/commandos/commandos.py
@@ -1,15 +1,17 @@
 # Dit bestand bevat alle commandos.
-from commandos.addCommando import addCommando
-from commandos.listCommando import listCommando
-from commandos.exitCommando import exitCommando
-from commandos.deleteCommando import deleteCommando
+from commandos.deleteCommando import *
+from commandos.listCommando   import *
+from commandos.exitCommando   import *
+from commandos.helpCommando   import *
+from commandos.addCommando    import *
 
 commandos = {
-  'add': addCommando,
-  'new': addCommando,
-  'ls': listCommando,
-  'list': listCommando,
-  'del': deleteCommando,
+  'help':   helpCommando,
+  'add':    addCommando,
+  'new':    addCommando,
+  'ls':     listCommando, # Zoals je ziet kunnen we hier twee commando entries dezelfde functies geven,
+  'list':   listCommando, # zodat zowel 'ls' als 'list' werken
+  'del':    deleteCommando,
   'delete': deleteCommando,
-  'exit': exitCommando
+  'exit':   exitCommando
 }
