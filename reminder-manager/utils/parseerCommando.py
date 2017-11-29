16b2e0203d558cc2f9b4b23f7ce7f23cf37aac0e
aetheryx <zain1@live.nl>
2017-11-29 22:35:13 +0100

    cleanup + add reference to help

--- reminder-manager/utils/parseerCommando.py
+++ reminder-manager/utils/parseerCommando.py
@@ -8,12 +8,19 @@ def parseerCommando ():
     kleurtjes['CYAAN'] # Door voor het eind van de output een ANSI-kleurcode te printen, 
   )                 # zal de input van de gebruiker met die kleur gekleurd zijn.
   commando = antwoord.split(' ')[0] # Het commando is aller eerste "woord" (het stukje van de string voor de eerste spatie).
-  if commando in commandos: # Als het herkende commando bestaat,
-    commandos[commando](antwoord.replace(commando, '').strip()) # roepen we de functie die bij dat commando hoort.
+  if commando in commandos: # Als het herkende commando bestaat,\
+    argumenten = antwoord[len(commando):len(antwoord) + 1].strip()
+    commandos[commando](argumenten) # roepen we de functie die bij dat commando hoort.
+    print() # Een lege regel.
   else: # Als dat niet zo is,
-    print( # vertellen we de gebruiker dat dat niet herkend is als een commando
+    print( # vertellen we de gebruiker dat dat niet herkend is als een commando, en dat ze help kunnen gebruiken.
       kleurtjes['RESET'] + kleurtjes['BOLD'] + kleurtjes['ROOD'] +
       commando +
       kleurtjes['RESET'] + kleurtjes['ROOD'] +
-      ' is niet herkend als een commando.'
+      ' is niet herkend als een commando.\nVoer' +
+      kleurtjes['CYAAN'] +
+      ' help ' +
+      kleurtjes['RESET'] + kleurtjes['ROOD'] +
+      'uit voor een lijst met commandos.' +
+      kleurtjes['ROOD']
     )
